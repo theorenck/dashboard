867a0ea5ad4f9826156b09d617a9cfaf3acd6568
Ícaro Heimig <icaro@icaromh.com>
2014-12-23 11:06:50 -0200

    - Adiconada index.html para o admin - crud api_servers - refactor de views - listagem de todos os items

--- public/assets/scripts/admin/api_server.js
+++ public/assets/scripts/admin/api_server.js
@@ -1,4 +1,4 @@
-var Atlas = angular.module('Atlas',['ngResource']);
+var Atlas = angular.module('Atlas',['ngResource', 'ngRoute']);
 
 /**
  * Configura o header para usar autenticação
@@ -10,9 +10,55 @@ Atlas.config([
     $httpProvider.defaults.useXDomain = true;
     $httpProvider.defaults.headers.common.Authorization = 'Token token=4361a34b6472e4634cd27f8d3f37108e';
   }
-
 ]);
 
+/**
+ * Configuração de todas
+ */
+Atlas.config(function($routeProvider){
+  $routeProvider
+
+  .when('/', {
+    url : '/',
+  })
+
+  .when('/api-server', {
+    controller: 'ApiServerController',
+    templateUrl: 'api_server.html'
+  })
+
+  .when('/dashboard', {
+    controller: 'dashboardController',
+    templateUrl: 'dashboard.html'
+  })
+
+  .when('/indicator', {
+    controller: 'indicatorController',
+    templateUrl: 'indicator.html'
+  })
+
+  .when('/permissions', {
+    controller: 'permissionsController',
+    templateUrl: 'permissions.html'
+  })
+
+  .when('/users', {
+    controller: 'usersController',
+    templateUrl: 'users.html'
+  })
+
+  .when('/widgets', {
+    controller: 'widgetsController',
+    templateUrl: 'widgets.html'
+  })
+
+  .otherwise({
+    redirectTo : '/'
+  });
+});
+
+
+/* RESOURCE APISERVERS */
 Atlas.factory(
   'ApiServers',
   ['$resource',
@@ -25,11 +71,86 @@ Atlas.factory(
   ]
 );
 
+/* RESOURCE DASHBOARDS */
+Atlas.factory(
+  'Dashboards',
+  ['$resource',
+
+    function($resource){
+      return $resource('http://127.0.0.1:9000/api/v1/dashboards/:id', { id: '@dashboard.id' }, {
+         'update': { method:'PUT' }
+      });
+    }
+  ]
+);
+
+/* RESOURCE INDICATORS */
+Atlas.factory(
+  'Indicators',
+  ['$resource',
+
+    function($resource){
+      return $resource('http://127.0.0.1:9000/api/v1/indicators/:id', { id: '@indicator.id' }, {
+         'update': { method:'PUT' }
+      });
+    }
+  ]
+);
+
+/* RESOURCE WIDGETS */
+Atlas.factory(
+  'Widgets',
+  ['$resource',
+
+    function($resource){
+      return $resource('http://127.0.0.1:9000/api/v1/widgets/:id', { id: '@widget.id' }, {
+         'update': { method:'PUT' }
+      });
+    }
+  ]
+);
+
+/* RESOURCE USERS */
+Atlas.factory(
+  'Users',
+  ['$resource',
+
+    function($resource){
+      return $resource('http://127.0.0.1:9000/api/v1/users/:id', { id: '@widget.id' }, {
+         'update': { method:'PUT' }
+      });
+    }
+  ]
+);
+
+/* RESOURCE PERMISSIONS */
+Atlas.factory(
+  'Permissions',
+  ['$resource',
+
+    function($resource){
+      return $resource('http://127.0.0.1:9000/api/v1/permissions/:id', { id: '@permission.id' }, {
+         'update': { method:'PUT' }
+      });
+    }
+  ]
+);
+
 
 /**
- * App Controller
+ * APP CONTROLLER
  */
 Atlas.controller('appController', [
+  '$scope',
+  function($scope){
+
+  }
+]);
+
+/**
+ * API SERVER CONTROLLER
+ */
+Atlas.controller('ApiServerController', [
   'ApiServers',
   '$scope',
 
@@ -71,11 +192,140 @@ Atlas.controller('appController', [
     }
 
     $scope.loadApiServer = function(server){
-      console.log(server);
       $scope.api_server = server;
     }
 
     $scope.renderList();
   }
 
+]);
+
+/**
+ * DASHBOARD CONTROLLER
+ */
+Atlas.controller('dashboardController', [
+  '$scope',
+  'Dashboards',
+
+  function($scope, Dashboards){
+    $scope.dashboard     = {};
+    $scope.dashboardList = [];
+
+    $scope.renderList = function(){
+      Dashboards.get(function(data){
+        $scope.dashboardList = data.dashboards;
+      });
+    }
+
+    $scope.loadDashboard = function(dash){
+      $scope.dashboard = dash;
+    }
+
+
+    $scope.renderList();
+  }
+]);
+
+/**
+ * INDICATOR CONTROLLER
+ */
+Atlas.controller('indicatorController', [
+  '$scope',
+  'Indicators',
+
+  function($scope, Indicators){
+    $scope.indicator     = {};
+    $scope.indicatorList = [];
+
+    $scope.renderList = function(){
+      Indicators.get(function(data){
+        $scope.indicatorList = data.indicators;
+      });
+    }
+
+    $scope.loadIndicator = function(item){
+      $scope.indicator = item;
+    }
+
+    $scope.renderList();
+  }
+]);
+
+/**
+ * WIDGET CONTROLLER
+ */
+Atlas.controller('widgetsController', [
+  '$scope',
+  'Widgets',
+
+  function($scope, Widgets){
+    $scope.widget     = {};
+    $scope.widgetList = [];
+
+    $scope.renderList = function(){
+      Widgets.get(function(data){
+        $scope.widgetList = data.widgets;
+      });
+    }
+
+    $scope.loadwidget = function(item){
+      $scope.widget = item;
+    }
+
+    $scope.renderList();
+  }
+]);
+
+
+/**
+ * USERS CONTROLLER
+ */
+Atlas.controller('usersController', [
+  '$scope',
+  'Users',
+
+  function($scope, Users){
+    $scope.user     = {};
+    $scope.userList = [];
+
+    $scope.renderList = function(){
+      Users.get(function(data){
+        console.log(data.users);
+        $scope.userList = data.users;
+      });
+    }
+
+    $scope.loadUser = function(item){
+      $scope.user = item;
+    }
+
+    $scope.renderList();
+  }
+]);
+
+
+/**
+ * USERS CONTROLLER
+ */
+Atlas.controller('permissionsController', [
+  '$scope',
+  'Permissions',
+
+  function($scope, Permissions){
+    $scope.permission     = {};
+    $scope.permissionList = [];
+
+    $scope.renderList = function(){
+      Permissions.get(function(data){
+        console.log(data.permissions);
+        $scope.permissionList = data.permissions;
+      });
+    }
+
+    $scope.loadpermission = function(item){
+      $scope.permission = item;
+    }
+
+    $scope.renderList();
+  }
 ]);
