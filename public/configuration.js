481cceaaf59d48dc64dc27c2137c7ecd21c1ad62
Ícaro Heimig <icaro@icaromh.com>
2015-04-16 12:48:29 -0300

    Adicionada configuracao default e normal

---
+++ public/configuration.js
@@ -0,0 +1,18 @@
+var Configuration = {
+
+  'time_to_refresh' : 1000 * 1000, // default: 30 seconds
+
+  'statement_server' : null,
+
+  'business_server'   : 'https://salty-sands-7468.herokuapp.com',
+  'middleware_server' : null,
+
+  'colors': {
+    'red'    : '#e74c3c',
+    'blue'   : '#3498db',
+    'orange' : '#f39c12',
+    'green'  : '#18bc9c',
+    'yellow' : '',
+    'purple' : ''
+  }
+};
